32824bf53d6418078468d742e22de31ec2f16587
KSH <ksh@codev.kr>
2018-10-27 16:37:55 +0900

    code reset
    1. code reset (junk code T.T)

---
+++ js/jquery.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+	$('#small_balloon').animate({opacity:'1'}, 1000);
+	$('#middle_balloon').animate({opacity:'1'}, 2500);
+	$('#big_balloon').animate({opacity:'1'}, 3500);
+	$('.skill_text').animate({opacity:'1'}, 4000);
+});
