d8c3b0fd434e08851a98bc90de31e8bac3505308
KSH <ksh@codev.kr>
2018-10-30 14:25:01 +0900

    code upgrade SEO
    1. robot.txt add
    2. index.html code develop
    3. css code develop

--- js/jquery.js
+++ js/jquery.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
-	$('#small_balloon').animate({opacity:'1'}, 1000);
+	$('#small_balloon').animate({opacity:'1'}, 1500);
 	$('#middle_balloon').animate({opacity:'1'}, 2500);
 	$('#big_balloon').animate({opacity:'1'}, 3500);
-	$('.skill_text').animate({opacity:'1'}, 4000);
 });
